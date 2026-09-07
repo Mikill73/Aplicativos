2b029a37f7b36398f4a08e2caa799b1937aab5fc
Mikill73 <109221027+Mikill73@users.noreply.github.com>
2026-09-07 20:35:51 -0300

    Create Compilar.java

---
+++ Academia/Compilar.java
@@ -0,0 +1,7 @@
+PACKAGE_NAME=com.academia.app
+MAIN_ACTIVITY=MainActivity
+SRC_PATH=src/main/java/com/academia/app
+MANIFEST_PATH=src/main/AndroidManifest.xml
+APK_NAME=AcademiaApp.apk
+ANDROID_SDK=android-28
+RES_PATH=res
