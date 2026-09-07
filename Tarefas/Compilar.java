efe268c08e26676afd07d4bf0093637b5ca00bad
Mikill73 <109221027+Mikill73@users.noreply.github.com>
2026-09-07 20:59:46 -0300

    Update Compilar.java

--- Tarefas/Compilar.java
+++ Tarefas/Compilar.java
@@ -1,1 +1,7 @@
-
+PACKAGE_NAME=com.task.app
+MAIN_ACTIVITY=MainActivity
+SRC_PATH=src/main/java/com/task/app
+MANIFEST_PATH=src/main/AndroidManifest.xml
+APK_NAME=Tarefas.apk
+ANDROID_SDK=android-28
+RES_PATH=res
