5c908fefcfe0b058aaf428577c152a19a0f9ec1d
Mikill73 <109221027+Mikill73@users.noreply.github.com>
2026-09-12 20:25:05 -0300

    Delete Caixinha directory

--- Caixinha/Compilar.java
+++
@@ -1,8 +0,0 @@
-PACKAGE_NAME=com.caixinha.app
-FOLDER_NAME=CaixinhaApp
-MAIN_ACTIVITY=MainActivity
-SRC_PATH=src/main/java/com/caixinha/app
-MANIFEST_PATH=src/main/AndroidManifest.xml
-APK_NAME=Caixinha.apk
-ANDROID_SDK=android-28
-RES_PATH=res
