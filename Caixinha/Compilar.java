fe2447c11a4510e11d67e054a25ecc28b70dceee
Mikill73 <109221027+Mikill73@users.noreply.github.com>
2026-09-12 20:25:23 -0300

    Add Compilar.java

---
+++ Caixinha/Compilar.java
@@ -0,0 +1,8 @@
+PACKAGE_NAME=com.caixinha.app
+FOLDER_NAME=CaixinhaApp
+MAIN_ACTIVITY=MainActivity
+SRC_PATH=src/main/java/com/caixinha/app
+MANIFEST_PATH=AndroidManifest.xml
+APK_NAME=Caixinha.apk
+ANDROID_SDK=android-28
+RES_PATH=src/main/res
